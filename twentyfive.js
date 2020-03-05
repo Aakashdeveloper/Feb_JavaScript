e198840e12472c8035dee36ae515d128361bbab9
aakashDeveloper <ahanda205@gmail.com>
2020-03-05 17:08:54 +0000

    a

---
+++ twentyfive.js
@@ -0,0 +1,25 @@
+document.getElementsByTagName('h1')
+HTMLCollection [h1]
+document.getElementsByTagName('h1')[0]
+<h1>​I'm AAKASH HANDA​</h1>​
+document.getElementsByTagName('h1')[0].innerText
+"I'm AAKASH HANDA"
+document.getElementsByClassName('scroll')
+HTMLCollection(4) [a.scroll, a.scroll, a.scroll, a.scroll]
+document.getElementsByTagName('h1')[0].innerText="NareshIT"
+"NareshIT"
+document.getElementsByClassName('scroll')
+HTMLCollection(3) [a.scroll, a.scroll, a.scroll]
+document.getElementById('scrolla')
+<a href=​"#skills" id=​"scrolla">​Skills​</a>​
+
+document.getElementById('scrolla').innerText
+"Skills"
+document.getElementById('scrolla').innerText="tech"
+"tech"
+document.getElementById('scrolla').style.color="Yellow"
+"Yellow"
+document.getElementById('scrolla').style.visibility="hidden"
+"hidden"
+document.getElementById('scrolla').style.visibility="visible"
+"visible"
---
+++ twentysix.js
@@ -0,0 +1,12 @@
+function color(){
+    alert("hiii")
+    document.getElementById('mytext').style.color="teal"
+}
+function visible(operation){
+    if(operation=='hide'){
+        document.getElementById('mytext').style.visibility="hidden"
+    }else{
+        document.getElementById('mytext').style.visibility="visible"
+    }
+    
+}
